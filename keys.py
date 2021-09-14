198be2d11ffff665aa499588907fc623e4613302
I Was Framed <87878121+iwasframed@users.noreply.github.com>
2021-09-14 15:34:39 -0700

    Initial Commit

---
+++ keys.py
@@ -0,0 +1,8 @@
+# keys_format.py shares the same format as keys.py.
+# This file is not meant to be used in the main script.
+# Instead, copy this file into keys.py and put your own
+# keys there.
+CONSUMER_KEY = 'API KEY'
+CONSUMER_SECRET = 'API SECRET'
+ACCESS_KEY = 'ACCESS TOKEN'
+ACCESS_SECRET = 'ACCESS SECRET'
